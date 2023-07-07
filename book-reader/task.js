c8ea1439cc74c3338fbb0c541cf34ab28d499630
Remezov Dmitriy <darknessdizi@gmail.com>
2023-07-08 05:12:14 +0800

    Решена задача 3 повышенной сложности

--- book-reader/task.js
+++ book-reader/task.js
@@ -1,22 +1,39 @@
 function replaceActive(event) {
     event.preventDefault();
+    const parent = this.parentElement;
+    const links = parent.querySelectorAll('a');
+    const linksArray = Array.from(links);
     const element = linksArray.find((item) => {
         return item.className.includes('active');
     });
-    element.classList.remove('font-size_active');
-    this.classList.add('font-size_active');
-    
-    const book = document.getElementById('book');
-    book.className = 'book';
-    if (this.dataset.size) {
-        book.classList.add(`book_fs-${this.dataset.size}`);
+
+    if (parent.className.includes('color')) {
+        const color = this.dataset.textColor;
+        replaceClassName('color_active', 'book_color-', color, element, this);
+    } else if (parent.className.includes('size')) {
+        const size = this.dataset.size;
+        replaceClassName('font-size_active', 'book_fs-', size, element, this);
+    } else {
+        const background = this.dataset.bgColor;
+        replaceClassName('color_active', 'book_bg-', background, element, this);
     }
 }
 
-const linksContent = document.querySelector('.book__control');
-const links = linksContent.querySelectorAll('a');
-const linksArray = Array.from(links);
+function replaceClassName(status, startClassName, atribute, element, obj) {
+    const book = document.getElementById('book');
+    const bookArray = Array.from(book.classList);
+    element.classList.remove(status);
+    obj.classList.add(status);
+    const nameClass = bookArray.find((item) => {
+        return item.includes(startClassName);
+    });
+    book.classList.remove(nameClass);
+    if (atribute) {
+        book.classList.add(`${startClassName}${atribute}`);
+    }
+}
 
+const links = document.querySelectorAll('a');
 links.forEach((item) => {
     item.addEventListener('click', replaceActive);
 });
